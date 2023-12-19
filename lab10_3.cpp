94c6a4b53d6c13b0c02654a8a92f517a2a0d7670
Your Name <you@example.com>
2023-12-19 15:44:19 +0700

    completed

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,28 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    int count = 0;
+    float sum = 0;
+    float sum2 =0 ;
+    float num;
+    string textline;
+    ifstream source("score.txt");
+    while(getline(source,textline)){
+            
+            num = atof(textline.c_str());
+            sum += num;
+            sum2 += pow(num,2);
+            count++;
+    }
+    cout << "Number of data = " << count << endl;
+    cout << setprecision(3);
+    cout << "Mean = " << sum/count << endl;
+    cout << "Standard deviation = " << sqrt((sum2/count)-pow((sum/count),2));
+}
